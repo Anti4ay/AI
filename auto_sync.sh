9d6a23214c2e3312d87eb5608b0687d6da03d976
Anti4ay <chukreevnr@gmail.com>
2026-05-08 17:55:27 +0500

    sync ИИ: 2026-05-08 17:55:27

--- auto_sync.sh
+++ auto_sync.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 SOURCE="/Users/nikitachukreev/Documents/ИИ"
 REPO="/Users/nikitachukreev/Documents/Система/AI"
-TARGET="$REPO/ИИ"
-
 cd "$REPO" || exit 1
 
 git pull origin main --rebase 2>&1
 
-rsync -av --delete "$SOURCE/" "$TARGET/" 2>&1
+rsync -av --delete --exclude='.git' --exclude='auto_sync.sh' "$SOURCE/" "$REPO/" 2>&1
 
 if [[ -n $(git status --porcelain) ]]; then
   git add -A
